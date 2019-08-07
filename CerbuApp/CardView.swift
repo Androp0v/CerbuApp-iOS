efc568fa5c79f929d7ed4170421e288e5b7fe064
Raúl Montón <raul_m_p@me.com>
2019-08-07 12:21:44 +0100

    Added highlighting animation to main view card interface

---
+++ CerbuApp/CardView.swift
@@ -0,0 +1,48 @@
+//
+//  CardView.swift
+//  CerbuApp
+//
+//  Created by Raúl Montón Pinillos on 07/08/2019.
+//  Copyright © 2019 Raúl Montón Pinillos. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        animate(isHighlighted: true)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        animate(isHighlighted: false)
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        animate(isHighlighted: false)
+    }
+    
+    private func animate(isHighlighted: Bool, completion: ((Bool) -> Void)?=nil) {
+        
+        
+        if isHighlighted {
+            UIView.animate(withDuration: 0.5,
+                           delay: 0,
+                           usingSpringWithDamping: 1,
+                           initialSpringVelocity: 0, animations: {
+                            self.transform = .init(scaleX: 0.96, y: 0.96)
+            }, completion: completion)
+        } else {
+            UIView.animate(withDuration: 0.5,
+                           delay: 0,
+                           usingSpringWithDamping: 1,
+                           initialSpringVelocity: 0, animations: {
+                            self.transform = .identity
+            }, completion: completion)
+        }
+    }
+
+}
