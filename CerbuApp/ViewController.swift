79fdb62c68dc72b8c397010039b203f23051a0bb
Raúl Montón <raul_m_p@me.com>
2019-09-19 13:01:17 +0200

    Fixed a bug where filters wouldn't clear properly when coming for the second time from the main UI.

--- CerbuApp/ViewController.swift
+++ CerbuApp/ViewController.swift
@@ -98,16 +98,6 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, UITableView
         mainTableView.delegate = self
         self.mainTableView.tableFooterView = UIView()
         
-        defaults.set(false, forKey: "soloAdjuntos")
-        defaults.set(false, forKey: "soloFavoritos")
-        defaults.set(true, forKey: "male")
-        defaults.set(true, forKey: "female")
-        defaults.set(true, forKey: "nbothers")
-        defaults.set(true, forKey: "100s")
-        defaults.set(true, forKey: "200s")
-        defaults.set(true, forKey: "300s")
-        defaults.set(true, forKey: "400s")
-        
         //Height of ParentContainer has to be corrected programatically for some reason. Sorry for hacky fix
         let correctedHeight = (self.view.frame.width - 40)*(1273.0/1920.0) + 40
         ParentContainer.frame = CGRect(x:0, y: 0, width: self.view.frame.width, height: correctedHeight)
@@ -131,6 +121,16 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, UITableView
         if gesture.state == .ended {
             //Only perform segue if tap is released inside button area (and not outside)
             if ContainerView.bounds.contains(touchLocation) {
+                //Clear filters
+                defaults.set(false, forKey: "soloAdjuntos")
+                defaults.set(false, forKey: "soloFavoritos")
+                defaults.set(true, forKey: "male")
+                defaults.set(true, forKey: "female")
+                defaults.set(true, forKey: "nbothers")
+                defaults.set(true, forKey: "100s")
+                defaults.set(true, forKey: "200s")
+                defaults.set(true, forKey: "300s")
+                defaults.set(true, forKey: "400s")
                 //Perform segue
                 self.performSegue(withIdentifier: "pushFromOrla", sender: self)
             }
