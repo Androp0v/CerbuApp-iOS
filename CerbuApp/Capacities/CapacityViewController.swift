2d90d4f48d1edfbedcc7aafe5cc30f9b13b104df
Raúl Montón Pinillos <raul_m_p@me.com>
2020-09-15 00:15:13 +0200

    Implemented progress bar data pulled from remote Firebase Realtime Database.

--- CerbuApp/Capacities/CapacityViewController.swift
+++ CerbuApp/Capacities/CapacityViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class CapacityViewController: UIViewController {
 
@@ -19,7 +20,17 @@ class CapacityViewController: UIViewController {
     @IBOutlet weak var bibliotecaContainerView: UIView!
     @IBOutlet weak var bibliotecaProgressBar: UIView!
     
+    let defaults = UserDefaults.standard
     
+    var comedorFractionNumber: Float = -1.0
+    var salaDeLecturaFractionNumber: Float = -1.0
+    var bibliotecaFractionNumber: Float = -1.0
+    
+    var databaseReference: DatabaseReference!
+    
+    var roomName = [String]()
+    var roomCapacityCurrent = [Int]()
+    var roomCapacityMax = [Int]()
     
     private func getProgressBarColor(fractionNumber: Float) -> UIColor{
         
@@ -36,18 +47,12 @@ class CapacityViewController: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        self.comedorContainerView?.layoutIfNeeded()
-        self.salaDeLecturaContainerView?.layoutIfNeeded()
-        self.bibliotecaContainerView?.layoutIfNeeded()
+    private func animateProgressBars(){
         
         // Create the constraints for the progressBars
-        let comedorConstraint = comedorProgressBar.widthAnchor.constraint(equalTo: comedorContainerView.widthAnchor, multiplier: 0.8)
-        let salaDeLecturaConstraint = salaDeLecturaProgressBar.widthAnchor.constraint(equalTo: salaDeLecturaContainerView.widthAnchor, multiplier: 0.25)
-        let bibliotecaConstraint = bibliotecaProgressBar.widthAnchor.constraint(equalTo: bibliotecaContainerView.widthAnchor, multiplier: 0.6)
+        let comedorConstraint = comedorProgressBar.widthAnchor.constraint(equalTo: comedorContainerView.widthAnchor, multiplier: CGFloat(max(0,comedorFractionNumber)))
+        let salaDeLecturaConstraint = salaDeLecturaProgressBar.widthAnchor.constraint(equalTo: salaDeLecturaContainerView.widthAnchor, multiplier: CGFloat(max(0,salaDeLecturaFractionNumber)))
+        let bibliotecaConstraint = bibliotecaProgressBar.widthAnchor.constraint(equalTo: bibliotecaContainerView.widthAnchor, multiplier: CGFloat(max(0,bibliotecaFractionNumber)))
         
         // Activate constraint(s)
         NSLayoutConstraint.activate([
@@ -56,21 +61,82 @@ class CapacityViewController: UIViewController {
             bibliotecaConstraint,
         ])
         
-        // Set progressbar colors
-        comedorProgressBar.backgroundColor = self.getProgressBarColor(fractionNumber: 0.8)
-        salaDeLecturaProgressBar.backgroundColor = self.getProgressBarColor(fractionNumber: 0.25)
-        bibliotecaProgressBar.backgroundColor = self.getProgressBarColor(fractionNumber: 0.6)
+        // Set initial progressbar colors
+        comedorProgressBar.backgroundColor = self.getProgressBarColor(fractionNumber: comedorFractionNumber)
+        salaDeLecturaProgressBar.backgroundColor = self.getProgressBarColor(fractionNumber: salaDeLecturaFractionNumber)
+        bibliotecaProgressBar.backgroundColor = self.getProgressBarColor(fractionNumber: bibliotecaFractionNumber)
         
-        // Animate progress bars
-        UIView.animate(withDuration: 0.8, animations: {
+        // Animate initial progress bar positions
+        UIView.animate(withDuration: Double(max(0,comedorFractionNumber)), animations: {
             self.comedorContainerView?.layoutIfNeeded()
             })
-        UIView.animate(withDuration: 0.25, animations: {
+        UIView.animate(withDuration: Double(max(0,salaDeLecturaFractionNumber)), animations: {
             self.salaDeLecturaContainerView?.layoutIfNeeded()
             })
-        UIView.animate(withDuration: 0.6, animations: {
+        UIView.animate(withDuration: Double(max(0,bibliotecaFractionNumber)), animations: {
             self.bibliotecaContainerView?.layoutIfNeeded()
             })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // Firebase Realtime Database reference
+        databaseReference = Database.database().reference().child("Capacities/Count")
+        
+        // Observe for changes in the database
+        databaseReference.queryLimited(toFirst: 10).observe(.value, with: { snapshot in
+                                                                
+            let value = snapshot.value as! [String: Any]
+            
+            self.roomName = []
+            self.roomCapacityCurrent = []
+            self.roomCapacityMax = []
+            
+            let sortedKeys = Array(value.keys).sorted(by: >)
+                        
+            for (room) in sortedKeys{
+                if room == "Comedor"{
+                    let currentCapacity = (value[room] as! [String: Int])["Current"] ?? 0
+                    let maxCapacity = (value[room] as! [String: Int])["Max"] ?? 0
+                    if maxCapacity == -1{
+                        self.comedorFractionNumber = -1
+                    }else{
+                        self.comedorFractionNumber = Float(currentCapacity)/Float(maxCapacity)
+                    }
+                }else if room == "SalaDeLectura"{
+                    let currentCapacity = (value[room] as! [String: Int])["Current"] ?? 0
+                    let maxCapacity = (value[room] as! [String: Int])["Max"] ?? 0
+                    if maxCapacity == -1{
+                        self.salaDeLecturaFractionNumber = -1
+                    }else{
+                        self.salaDeLecturaFractionNumber = Float(currentCapacity)/Float(maxCapacity)
+                    }
+                }else if room == "Biblioteca"{
+                    let currentCapacity = (value[room] as! [String: Int])["Current"] ?? 0
+                    let maxCapacity = (value[room] as! [String: Int])["Max"] ?? 0
+                    if maxCapacity == -1{
+                        self.bibliotecaFractionNumber = -1
+                    }else{
+                        self.bibliotecaFractionNumber = Float(currentCapacity)/Float(maxCapacity)
+                    }
+                }
+            }
+            
+            // Animate progress bars after database changes
+            self.animateProgressBars()
+            
+        })
+        
+        // Fix weird layout constraints mistakenly being animated
+        self.comedorContainerView?.layoutIfNeeded()
+        self.salaDeLecturaContainerView?.layoutIfNeeded()
+        self.bibliotecaContainerView?.layoutIfNeeded()
+        
+        // Animate progressbars with initial values
+        animateProgressBars()
     }
     
 
