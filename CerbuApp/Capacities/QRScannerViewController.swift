1d4ca63908ced43486b1907c85723b77a7d29b31
Raúl Montón Pinillos <raul_m_p@me.com>
2020-09-14 14:17:27 +0200

    Refactoring of QR scanning code.

---
+++ CerbuApp/Capacities/QRScannerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QRScannerViewController.swift
+//  CerbuApp
+//
+//  Created by Raúl Montón Pinillos on 14/9/20.
+//  Copyright © 2020 Raúl Montón Pinillos. All rights reserved.
+//
+
+import UIKit
+
+class QRScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
